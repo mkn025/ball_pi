b2ca0b0319c28e2419d6a6308bb4030da6727d14
mkn025 <78483320+mkn025@users.noreply.github.com>
2022-03-26 18:04:03 +0100

    main

--- kode/Ball_pi.py
+++ kode/Ball_pi.py
@@ -1,3 +1,4 @@
+# Ball_pi med rami sine forberinger
 
 import pygame
 pygame.init()
@@ -5,7 +6,7 @@ pygame.font.init()
 
 # Viktige varibler
 x_vin,y_vin = (1280),(720)
-fps = 1200
+fps = 12000000
 
 # Farger
 Bakgrunn = (30,30,30)
@@ -30,17 +31,18 @@ def liten_ball(x_kod, y_kod, radius):
     pygame.draw.circle(vindu, ball_farge, (x_kod,y_kod), radius, width=0)
 
 
-
+hundre_potens = float(input("noe: "))
+noe2 = hundre_potens*2
 # bevegelses varibler
 
     # stor ball
-radius_stor_ball = 100
+radius_stor_ball = 50 * (noe2)
 stor_ball_pos_x = 700
 stor_ball_pos_y = 500 - radius_stor_ball
 
 
     # liten ball 
-radius_liten_ball = 50
+radius_liten_ball = 25 * (noe2)
 liten_ball_pos_x = 0 + radius_liten_ball + 100
 liten_ball_pos_y = stor_ball_pos_y + radius_stor_ball - radius_liten_ball
 
@@ -48,12 +50,13 @@ liten_ball_pos_y = stor_ball_pos_y + radius_stor_ball - radius_liten_ball
 
     # Fysikk variabler liten ball 
 
-v2_start = 2 # farten stor ball
-m2 = 1
 
+v2_start = -2 # farten stor ball
+m2 = 100 * (10**(noe2))
 
 v1_start = 0   # farten liten ball
 m1 = 1
+
 
 
 # tekst
@@ -66,13 +69,11 @@ def tekst(x,y,varibler, tektst):
 
 # telling av kolisjon
 Antall_kolisjoner = 0  
- 
-
-
 
 # kolidering
 kolisjon = False
 kolisjon_med_vegg = False
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -81,63 +82,49 @@ while True:
     bakke(blue)
 
     # Grunnbevegelse 
-    stor_ball_pos_x -= v2_start
+    stor_ball_pos_x += v2_start #endret fra - til +
     liten_ball_pos_x += v1_start
 
-    stor_ball_pos_x - radius_stor_ball > liten_ball_pos_x + radius_liten_ball
     # støt med annen ball 
-    if stor_ball_pos_x - radius_stor_ball == liten_ball_pos_x + radius_liten_ball:
+    if (stor_ball_pos_x - radius_stor_ball) <= (liten_ball_pos_x + radius_liten_ball): #endret == til <=
         kolisjon = True
         Antall_kolisjoner += 1
-
-    
     else:
         kolisjon = False
 
     # tekst som vises i bilde
     tekst(1000,100,Antall_kolisjoner, "Antall treff")
-    
     print(v2_start,v1_start)
     if kolisjon == True:
         # endring av fart og retning
-
-       # Antall_kolisjoner += 1
-
         
         
         # utregninger for elastisk kolisjon
-       
-       
-
 
         sum_av_M = m2 + m1
         
-        v2 = (((m2 - m1)/(sum_av_M))*v2_start) + (((2*m1)/(sum_av_M))* v1_start)
-        v1 = (((m1 - m2)/(sum_av_M))*v1_start) + (((2*m2)/(sum_av_M))*v2_start)
+        v2 = ((((m2-m1)*v2_start)+(2*m1*v1_start))/(sum_av_M))
+        v1 = ((((m1-m2)*v1_start)+(2*m2*v2_start))/(sum_av_M))
 
-        v1_start = -v1
+        v1_start = v1 
         v2_start = v2
+        kolisjon = False
         # Endring av fart uten fysikk
 
     elif kolisjon_med_vegg == True:
         Antall_kolisjoner += 1 
         kolisjon_med_vegg = False
 
-
- # støt med vegg
-    if stor_ball_pos_x > x_vin - radius_stor_ball:
-        v2_start = v2_start
-        
-
-    elif liten_ball_pos_x < 0 + radius_liten_ball:
+ # støt med veg
+    if liten_ball_pos_x < 0 + radius_liten_ball:
         v1_start = -v1_start
         kolisjon_med_vegg = True
     
-
-
     # rendre ballene 
     stor_ball(stor_ball_pos_x,stor_ball_pos_y,radius_stor_ball)
     liten_ball(liten_ball_pos_x,liten_ball_pos_y,radius_liten_ball)
-    
+
+    # tekst som vises i bilde
+    tekst(1000,100,Antall_kolisjoner, "Antall treff")    
 
     pygame.display.update()
