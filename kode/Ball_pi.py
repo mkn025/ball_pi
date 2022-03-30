d3741d2d2affb0c103dde1f99137fbfbb6985f8f
Bestevenn <ramialsalman2004@hotmail.com>
2022-03-30 19:56:30 +0200

    Update Ball_pi.py

--- kode/Ball_pi.py
+++ kode/Ball_pi.py
@@ -59,7 +59,7 @@ liten_ball_pos_y = stor_ball_pos_y + radius_stor_ball - radius_liten_ball
 
 # Fysikk variabler baller  
 
-v2_start = -2 # farten stor ball
+v2_start = -5 # farten stor ball
 m2 = 0.01 * (100**(antall_siffer))
 
 v1_start = 0   # farten liten ball
