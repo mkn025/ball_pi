454f1e41f5a658459adf6739a7ff4bb5fa517428
Bestevenn <ramialsalman2004@hotmail.com>
2022-04-08 09:12:17 +0200

    main

--- kode/Forklaring.py
+++ kode/Forklaring.py
@@ -17,22 +17,29 @@ vindu = pygame.display.set_mode((x_vin,y_vin))
 def bakke(farge):
     pygame.draw.rect(vindu, farge, (0,500,x_vin,y_vin), width=0)
 
-
-# parametere for sikel
+# parametere for stor sikel
 radius_stor_sirkel = 200
-
 
 # paramentre for liten ball 
 radius_liten_ball = 10
+x_pos_liten_ball, y_pos_liten_ball = x_vin/2-radius_stor_sirkel, y_vin/2
+
+bevegelses = True
+
+dx = 1
+dy = 1
+
+#Definisjon av ting
+def bakgrunn():
+    vindu.fill(Bakgrunn) 
 
 def linjer(start_pos, end_pos,width):
     pygame.draw.line(vindu, white, (start_pos), (end_pos), width=width)
 
-x_pos_liten_ball, y_pos_liten_ball = x_vin/2-radius_stor_sirkel, y_vin/2
+def sirkel(Kordinater,radius,width):
+    pygame.draw.circle(vindu,blue, (Kordinater),radius,width=width)
 
-dx = 1
-dy = 1
-    
+#program    
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,31 +47,28 @@ while True:
             quit()
     
 
-    #  make the ball move around the circle
+    #Bevegelse
     if bevegelses:
         x_pos_liten_ball += dx
-    y_pos_liten_ball += dx
+    y_pos_liten_ball += dy
     if x_pos_liten_ball == (x_vin/2) and y_pos_liten_ball == (y_vin/2 + radius_stor_sirkel):
         bevegelses = False
         dy = -dy
-        
+
+    #Vindu farge
+    bakgrunn()
+
+    #Stor sikel
+    sirkel((x_vin/2,y_vin/2),radius_stor_sirkel,1)
     
-
-
-    vindu.fill(Bakgrunn)
-    # hele sikel
-    pygame.draw.circle(vindu, blue, (x_vin/2,y_vin/2), radius_stor_sirkel, width=1)
-    pygame.draw.circle(vindu, blue, (x_vin/2-radius_stor_sirkel,y_vin/2), radius_liten_ball, width=0)
-    
-
     # liten ball som skal kolidere
-    pygame.draw.circle(vindu, blue, (x_pos_liten_ball,y_pos_liten_ball), radius_liten_ball, width=0)
-
+    sirkel((x_pos_liten_ball,y_pos_liten_ball),radius_liten_ball,0)
+    
     # vannrett linje
     linjer((0,y_vin/2),(x_vin,y_vin/2),1)
+    
     # loddrett linje
     linjer((x_vin/2,0),(x_vin/2,y_vin),(1))
     
 
     pygame.display.update()
-
