6833727a036dac5566d04b22529eeab015a68d96
mkn025 <78483320+mkn025@users.noreply.github.com>
2022-04-08 08:31:12 +0200

    main

--- kode/Forklaring.py
+++ kode/Forklaring.py
@@ -9,6 +9,7 @@ svart = (0,0,0)
 blue = (125, 177, 244)
 green = (0,255,0)
 ball_farge  = (95, 137, 140)
+white = (255,255,255)
 
 
 vindu = pygame.display.set_mode((x_vin,y_vin))
@@ -16,6 +17,14 @@ vindu = pygame.display.set_mode((x_vin,y_vin))
 # elementer 
 def bakke(farge):
     pygame.draw.rect(vindu, farge, (0,500,x_vin,y_vin), width=0)
+
+
+# parametere for sikel
+radius_stor_sirkel = 200
+
+
+# paramentre for ball 
+radius_liten_ball = 10
 
 
 
@@ -27,7 +36,13 @@ while True:
     
 
     vindu.fill(Bakgrunn)
-    bakke(green)
+    pygame.draw.circle(vindu, blue, (x_vin/2,y_vin/2), radius_stor_sirkel, width=1)
+    pygame.draw.circle(vindu, blue, (x_vin/2-radius_stor_sirkel,y_vin/2), radius_liten_ball, width=0)
+    # vannrett linje
+    pygame.draw.line(vindu, white, (0,y_vin/2), (x_vin,y_vin/2), width=1)
+    # loddrett linje
+    pygame.draw.line(vindu, white, (x_vin/2,0), (x_vin/2,y_vin), width=1)
+
 
     pygame.display.update()
 
