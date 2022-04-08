5e3f8998fc40488b2fca3405b1e34148aeedbdab
Bestevenn <ramialsalman2004@hotmail.com>
2022-04-08 08:56:49 +0200

    Merge branch 'main' of https://github.com/mkn025/ball_pi

--- kode/Forklaring.py
+++ kode/Forklaring.py
@@ -38,6 +38,17 @@ while True:
             quit()
     
 
+    #  make the ball move around the circle
+    if bevegelses:
+        x_pos_liten_ball += dx
+    y_pos_liten_ball += dx
+    if x_pos_liten_ball == (x_vin/2) and y_pos_liten_ball == (y_vin/2 + radius_stor_sirkel):
+        bevegelses = False
+        dy = -dy
+        
+    
+
+
     vindu.fill(Bakgrunn)
     # hele sikel
     pygame.draw.circle(vindu, blue, (x_vin/2,y_vin/2), radius_stor_sirkel, width=1)
@@ -47,7 +58,10 @@ while True:
     # liten ball som skal kolidere
     pygame.draw.circle(vindu, blue, (x_pos_liten_ball,y_pos_liten_ball), radius_liten_ball, width=0)
 
-  
+    # vannrett linje
+    linjer((0,y_vin/2),(x_vin,y_vin/2),1)
+    # loddrett linje
+    linjer((x_vin/2,0),(x_vin/2,y_vin),(1))
     
 
     pygame.display.update()
