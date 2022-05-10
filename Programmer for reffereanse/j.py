baaee106d69343148aae470820dea1ff34ccd78f
mkn025 <78483320+mkn025@users.noreply.github.com>
2022-05-10 12:57:12 +0200

    main

--- Programmer for reffereanse/j.py
+++ Programmer for reffereanse/j.py
@@ -1,8 +1,11 @@
-import pygame
-pygame.init()
-screen = pygame.display.set_mode((640, 480))
-font = pygame.font.Font(pygame.font.get_default_font(), 36)
 
-# now print the text
-text_surface = font.render('Hello world', antialias=True, color=(0, 0, 0))
-screen.blit(text_surface, dest=(0,0))
+
+
+
+import os
+
+
+input1 = input("tall:")
+
+if input1 == "1":
+    os.system("python3 kode/kube_pi_forklaring.py")
