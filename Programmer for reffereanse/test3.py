acb067faca9bbaaa8fde5922b444c387ae5d22bf
mkn025 <78483320+mkn025@users.noreply.github.com>
2022-05-19 19:06:16 +0200

    main

--- Programmer for reffereanse/test3.py
+++ Programmer for reffereanse/test3.py
@@ -1,12 +1,6 @@
-import pygame
-white = (255,255,255)
-pygame.init()
-vindu = pygame.display.set_mode((800,600))
+import random
 
 
-
-
-def tekst(x,y,varibler, tekst, font_størrelse):
-    font = pygame.font.SysFont('arial', font_størrelse)
-    tekst_som_vises = font.render(f"{varibler} {tekst} ",True,white)
-    vindu.blit(tekst_som_vises,(x, y))
+while True:
+    t = input("")
+    print(random.randint(1,6))
